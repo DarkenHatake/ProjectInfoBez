3c94b0fab8eb850fa16d25712447d519d313df18
DarkenHatake <akaldertop05@gmail.com>
2025-03-19 12:09:18 +0700

    go в папке bac

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Login from "./Login";
+import Register from "./Register";
 
 const Main = () => {
     return (
-        <h1>Дашборд</h1>
+        /*<h1>Дашборд</h1>*/
+        <div>
+            <Login />
+            <Register />
+        </div>
     );
 
 };
