54f38175b1505615a5731d3af1b3968da5c4753d
Unide <kira.onni@mail.ru>
2025-03-25 12:51:42 +0700

    added and updated entity

--- subject_task.go
+++ subject_task.go
@@ -2,13 +2,12 @@ package ProjectInfoBez
 
 import "time"
 
-type Personal_task struct {
+type Subject_Task struct {
 	Id          string    `json:"id"`
 	Title       string    `json:"title"`
 	Description string    `json:"description"`
+	Created_At  string    `json:"created_At"`
 	Deadline    time.Time `json:"deadline"`
 	Is_done     bool      `json:"is_done"`
 	Is_passed   bool      `json:"is_passed"`
-	Created_at  string    `json:"created_at"`
-	User_ID     int       `json:"user_id"`
 }
