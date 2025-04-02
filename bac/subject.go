ec19e49fc8b5d66441cff43b72ea37b8824a407d
Yulliash <yuliamut666@gmail.com>
2025-04-02 22:21:21 +0700

    Удалил папки bac и pkg

--- bac/subject.go
+++
@@ -1,8 +0,0 @@
-package ProjectInfoBez
-
-type Subject struct {
-	Id         string `json:"id"`
-	Title      string `json:"title"`
-	Created_at string `json:"created_at"`
-	User_ID    int    `json:"user_id"`
-}
--- bac/subscription.go
+++
@@ -1,7 +0,0 @@
-package ProjectInfoBez
-
-type Subscription struct {
-	Id         string `json:"id"`
-	User_Id    string `json:"user_id"`
-	Subject_Id string `json:"subject_id"`
-}
