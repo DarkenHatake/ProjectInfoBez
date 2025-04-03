56f651d244f633064eae038bc1023cca1c0f7711
DarkenHatake <akaldertop05@gmail.com>
2025-04-03 13:16:21 +0700

    часть пофиксил

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,10 +7,12 @@ const TaskList = ({ tasks, onEdit, onDelete }) => {
             <ul>
                 {tasks.map((task, index) => (
                     <li key={index} className="task-item">
-                        <p className="task-title">{task.title}</p>
-                        <p className="task-description">Описание:{task.description}</p>
-                        <p className="task-deadline">Дедлайн выполнения: {task.deadlineStart}</p>
-                        <p className="task-deadline">Дедлайн сдачи: {task.deadlineEnd}</p>
+                        <div className="task-headr">
+                            <p className="task-title">{task.title}</p>
+                            <p className="task-description">Описание:{task.description}</p>
+                            <p className="task-deadline-1">Дата выполнения: {task.deadlineStart}</p>
+                            <p className="task-deadline-2">Дата сдачи: {task.deadlineEnd}</p>
+                        </div>
                         <button className="edit-button" onClick={() => onEdit(task)}>Изменить</button>
                         <button className="delete-button" onClick={() => onDelete(task)}>Удалить</button>
                     </li>
