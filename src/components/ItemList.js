4ab32273ad6a42c608e4063769a62253c17eb867
DarkenHatake <akaldertop05@gmail.com>
2025-04-04 17:43:46 +0700

    merge with Julia second

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,24 +4,24 @@ import './ComponentsStyles/ItemList.css'; // Импортируйте CSS-фай
 const ItemList = ({ items, onDelete }) => {
     return (
         <div className="item-list-container">
-                <ul className="item-list">
-                    {items.map((item, index) => (
-                        <li key={index} className="item">
-                            <div className="item-headr">
-                                <h3 className="item-name">{item.name}</h3>
-                                {item.description && (
-                                    <p className="item-description"><strong>Описание:</strong> {item.description}</p>
-                                )}
-                                {item.deadline && (
-                                    <p className="item-deadline"><strong>Дедлайн:</strong> {item.deadline}</p>
-                                )}
-                            </div>
-                            <button className="delete-button" onClick={() => onDelete(index)}>Удалить</button>
-                            <button className="go-button">Перейти</button>
+            <ul className="item-list">
+                {items.map((item, index) => (
+                    <li key={index} className="item">
+                        <div className="item-headr">
+                            <h3 className="item-name">{item.name}</h3>
+                            {item.description && (
+                                <p className="item-description"><strong>Описание:</strong> {item.description}</p>
+                            )}
+                            {item.deadline && (
+                                <p className="item-deadline"><strong>Дедлайн:</strong> {item.deadline}</p>
+                            )}
+                        </div>
+                        <button className="delete-button" onClick={() => onDelete(index)}>Удалить</button>
+                        <button className="go-button">Перейти</button>
 
-                        </li>
-                    ))}
-                </ul>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 };
